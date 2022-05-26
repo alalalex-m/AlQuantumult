9887283f9e3885ea01091f98f5c935a009013305
Alexander McAnderw <alexandermcandrewog@gmail.com>
2022-05-26 12:07:40 +0800

    raindrop

---
+++ Scripts/Raindrop.js
@@ -0,0 +1,18 @@
+/*
+
+[rewrite_local]
+  
+# Cubox
+ ^https:\/\/cubox.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/linuszlx/JS/main/Crack/cubox.js
+
+[mitm]
+cubox.pro
+
+*/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+obj.data.payTime = "2021-01-01T08:00:00+08:00";
+obj.data.isExpire = false;
+obj.data.expireTime = "2039-01-01T08:00:00+08:00";
+$done({body: JSON.stringify(obj)});
