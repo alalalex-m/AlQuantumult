27e333dbd0253c01cb95df7364236916dde2c2a3
AlxXxlA <alexandermcandrewog@gmail.com>
2023-11-03 11:42:31 +0800

    rev test

--- Scripts/revenueCat.js
+++ Scripts/revenueCat.js
@@ -1,34 +1,60 @@
-// Combined script with UA detection
-const userAgent = $request.headers['User-Agent'];
+/*
 
-if (userAgent.includes('CountDuck')) {
-    /*
+Revenuecat解锁合集，已解锁APP及下载地址请见说明：https://github.com/Guding88/Script/blob/main/Readme.md
 
-    [rewrite_local]
-
-    U-A：CountDuck
-     
-    # 倒数鸭
-     ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/alalalex-m/AlQuantumult/main/Scripts/CountdownDuck.js
-     ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/alalalex-m/AlQuantumult/main/Scripts/CountdownDuck.js
-
-    [mitm]
-    hostname=api.revenuecat.com
-
-    */
-} if (userAgent.includes('Aphrodite')) {
-/*    # Aphrodite
-    ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-echo-response https://raw.githubusercontent.com/alalalex-m/AlQuantumult/main/Scripts/Aphrodite.js
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/APPheji_Revenuecat.js
 
 
-    [mitm]
+[MITM]
+hostname = api.revenuecat.com
 
-    hostname = api.revenuecat.com
-}
 */
-    else {
-  // Script 1
-  // *******************************
-  
-}
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlements = guding6.subscriber.entitlements || {};
+  var headers = {};
+  for (var key in $request.headers) {
+  const reg = /^[a-z]+$/;
+  if (key === "User-Agent" && !reg.test(key)) {
+    var lowerkey = key.toLowerCase();
+    $request.headers[lowerkey] = $request.headers[key];
+    delete $request.headers[key];
+    }
+  }
+  var UA = $request.headers['user-agent'];
+  const app = 'gd';
+  const UAMappings = {
+    'CountDuck':{ name: 'premium', id: 'Lifetime'},
+    'Aphrodite':{ name: 'premium', id: 'com.ziheng.aphrodite.lifetime'},
 
+
+    
+    
+  };
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-06-06T06:06:06Z",
+    "purchase_date": "2023-06-06T06:06:06Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+  for (const i in UAMappings) {
+    if (new RegExp(`^${i}`, 'i').test(UA)) {
+      const { name, id } = UAMappings[i];
+      guding6.subscriber.subscriptions = {};
+      guding6.subscriber.subscriptions[id] = data;
+      guding6.subscriber.entitlements[name] = JSON.parse(JSON.stringify(data));
+      guding6.subscriber.entitlements[name].product_identifier = id;
+      break;
+    }
+  }
+  guding.body = JSON.stringify(guding6);
+}
+$done(guding);
---
+++ Scripts/xxxx.js
@@ -0,0 +1,89 @@
+{
+  "Author R_E" : "作者 R_E",
+  "request_date_ms" : 1591358081473,
+  "Congratulation on grabbing subscription data" : "恭喜您抓取到订阅源数据",
+  "request_date" : "2020-06-05T11:54:41Z",
+  "subscriber" : {
+    "non_subscriptions" : {
+      "all" : [
+        {
+          "id" : "123",
+          "is_sandbox" : false,
+          "purchase_date" : "2019-12-01T00:00:00Z",
+          "original_purchase_date" : "2019-12-01T00:00:00Z",
+          "product_identifier" : "com.ziheng.aphrodite.lifetime",
+          "store" : "app_store"
+        }
+      ],
+      "com.ziheng.aphrodite.lifetime" : [
+        {
+          "id" : "123",
+          "is_sandbox" : false,
+          "purchase_date" : "2019-12-01T00:00:00Z",
+          "original_purchase_date" : "2019-12-01T00:00:00Z",
+          "product_identifier" : "com.ziheng.aphrodite.lifetime",
+          "store" : "app_store"
+        }
+      ]
+    },
+    "first_seen" : "2000-01-01T01:01:01Z",
+    "original_application_version" : "20",
+    "other_purchases" : {
+      "com.ziheng.aphrodite.lifetime" : {
+        "is_sandbox" : false,
+        "period_type" : "normal",
+        "billing_issues_detected_at" : null,
+        "unsubscribe_detected_at" : null,
+        "expires_date" : "2030-01-01T01:01:01Z",
+        "grace_period_expires_date" : null,
+        "original_purchase_date" : "2020-01-01T01:01:01Z",
+        "purchase_date" : "2020-01-01T01:01:01Z",
+        "store" : "app_store"
+      }
+    },
+    "management_url" : "itms-apps://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "com.ziheng.aphrodite.lifetime" : {
+        "is_sandbox" : false,
+        "period_type" : "normal",
+        "billing_issues_detected_at" : null,
+        "unsubscribe_detected_at" : null,
+        "expires_date" : "2030-01-01T01:01:01Z",
+        "grace_period_expires_date" : null,
+        "original_purchase_date" : "2020-01-01T01:01:01Z",
+        "purchase_date" : "2020-01-01T01:01:01Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+      "all" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2020-01-01T01:01:01Z",
+        "product_identifier" : "com.ziheng.aphrodite.lifetime",
+        "expires_date" : "2030-01-01T01:01:01Z"
+      },
+      "com.ziheng.aphrodite.lifetime" : {
+        "is_sandbox" : false,
+        "period_type" : "normal",
+        "billing_issues_detected_at" : null,
+        "unsubscribe_detected_at" : null,
+        "expires_date" : "2030-01-01T01:01:01Z",
+        "grace_period_expires_date" : null,
+        "product_identifier" : "com.ziheng.aphrodite.lifetime",
+        "original_purchase_date" : "2020-01-01T01:01:01Z",
+        "purchase_date" : "2020-01-01T01:01:01Z",
+        "store" : "app_store"
+      }
+    },
+    "original_purchase_date" : "2000-01-01T01:01:01Z",
+    "original_app_user_id" : "0099LK05-2799-489J-09CD-D9IJSEB09876",
+    "last_seen" : "2030-01-01T01:01:01Z"
+  },
+  "Commercial use prohibited" : "禁止商业用途",
+  "你可以加入群组获取帮助" : "https://t.me/yqc_777",
+  "For learning reference only" : "仅供学习参考",
+  "You own the right to use it, no doubt about it but not to share" : "您拥有使用权这是毋庸置疑的且不可以分享",
+  "But you may not use this data without attribution" : "但你不可以使用本数据却不注明出处",
+  "You can join the group for help" : "https://t.me/yqc_777"
+}
+
