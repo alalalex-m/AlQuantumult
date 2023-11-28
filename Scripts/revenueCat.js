c66c872508afcd588c750f4d61f8b9445da02be1
AlxXxlA <alexandermcandrewog@gmail.com>
2023-11-28 23:37:35 +0800

    x

--- Scripts/revenueCat.js
+++ Scripts/revenueCat.js
@@ -39,6 +39,7 @@ if (typeof $response == "undefined") {
       'Planny':{ name: 'premium', id: 'AlxXxlA'},
       'OneBox':{ name: 'all', id: 'com.ziheng.OneBox'},
       'Awesome%20Habits':{ name: 'HabitsPremiumLifetime', id: '$rc_lifetime'},
+      'apollo':{ name: 'all', id: 'com.alxxxla.premium.yearly'},
 
     
     
